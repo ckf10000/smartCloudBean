4f6ea7146f47cf9ec82dbccd1ae7ab2d159f6e3e
ckf10000 <ckf10000@sina.com>
2024-04-09 09:54:12 +0800

    first

---
+++ main.go
@@ -0,0 +1,27 @@
+// Package main
+/***********************************************************************************************************************
+* ProjectName:  smartCloudBean
+* FileName:     main.go
+* Description:  TODO
+* Author:       ckf10000
+* CreateDate:   2024/04/02 10:43:04
+* Copyright ©2011-2024. Hunan xyz Company limited. All rights reserved.
+* *********************************************************************************************************************/
+package main
+
+import (
+	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/net/ghttp"
+	"github.com/gogf/gf/v2/os/gctx"
+)
+
+func main() {
+
+	s := g.Server()
+	s.BindHandler("/", func(r *ghttp.Request) {
+		r.Response.write("你好，我的世界！")
+	})
+
+	s.Run(gctx.New())
+
+}
